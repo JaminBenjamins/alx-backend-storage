7bae3dc96c201fa45082d76cd6febb64c03d0764
JaminBenjamins <jaminbenjamins@gmail.com>
2023-10-13 00:22:57 +0300

    Validate emaal input if it changes

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- Script that creates a trigger that resets the attribute
+-- valid_email only when it has changed.
+DELIMETER $$ ;
+CREATE TRIGGER validate BEFORE UPDATE ON users 
+FOR REACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;$$
+delimiter;
